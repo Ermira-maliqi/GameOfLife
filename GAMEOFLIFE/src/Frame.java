1ac7e33765f8cf6b150df426dcc2e359d3daaa36
Ermira-maliqi <57395076+Ermira-maliqi@users.noreply.github.com>
2020-05-13 20:58:54 +0200

    Update Frame.java

--- GAMEOFLIFE/src/Frame.java
+++ GAMEOFLIFE/src/Frame.java
@@ -1,17 +1,16 @@
-import java.awt.Color;
-
 import javax.swing.*;
 
 public class Frame {
 		
-	Panel p = new Panel();
-
 			public Frame() {
 	    	JFrame f = new JFrame();
 	    	f.setSize(415,480);
 			f.setVisible(true);
 			f.setTitle("                                 Game of Life");
 			f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+			Panel p = new Panel();
+
 			f.getContentPane().add(p);
 	    
 		}
